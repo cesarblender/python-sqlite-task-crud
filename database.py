bd67a74c5b3c5784eb659316182f7309d4ff6258
seokkuuu <seokkub@gmail.com>
2023-08-17 18:20:55 -0400

    Created README

--- database.py
+++ database.py
@@ -1,8 +1,10 @@
 import sqlite3
 
+# Connection to the ./tasks.db database
 conn = sqlite3.connect('tasks.db')
 cursor = conn.cursor()
 
+# Create the "tasks" table if not exists
 cursor.execute('''
     CREATE TABLE IF NOT EXISTS tasks (
         id INTEGER PRIMARY KEY,
@@ -11,4 +13,5 @@ cursor.execute('''
     )
 ''')
 
+# Save changes
 conn.commit()
