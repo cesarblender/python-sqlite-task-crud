cd0e263f1bb397f48a6eeeb771946b379ccec588
seokkuuu <seokkub@gmail.com>
2023-08-17 18:15:58 -0400

    Added comments to main.py

--- main.py
+++ main.py
@@ -2,7 +2,11 @@ from database import conn
 from task_list import TaskList
 
 if __name__ == "__main__":
+    # Instantiate the task list class
     task_list = TaskList()
+
+    # Show the menu options
     task_list.start()
 
+# Close the database connection
 conn.close()
